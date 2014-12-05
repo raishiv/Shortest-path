7706beea9e735a0f7825e36130ebad22ffa31d63
Shiv <raishiv@gmail.com>
2014-12-04 21:19:15 -0600

    Initial Commit

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dijkstra
+//
+//  Created by Shiv chandra Kumar on 12/4/14.
+//  Copyright (c) 2014 Shiv chandra Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
